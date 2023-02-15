83a6f5ab6e61fda59395073d10a943f4ff66e5c6
Sourisse Gaetan <114146959+GaetanSourisse@users.noreply.github.com>
2023-02-15 14:30:16 +0100

    Add files via upload

---
+++ wall.php
@@ -0,0 +1,172 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Mur</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+    <?php include_once('header.php'); ?>
+    <?php include('connexion.php'); ?>
+
+    <div id="wrapper">
+        <?php
+        $userId = intval($_GET['user_id']);
+        ?>
+        <aside>
+            <?php
+            /**
+             * Etape 3: récupérer le nom de l'utilisateur
+             */
+            $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            $user = $lesInformations->fetch_assoc();
+            //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
+            //echo "<pre>" . print_r($user, 1) . "</pre>";
+            ?>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <section>
+                <h3>Présentation</h3>
+                <p>Sur cette page vous trouverez tous les message de l'utilisatrice :
+                    <?php echo $user['alias'] ?>
+                    (n°
+                    <?php echo $userId ?>)
+                </p>
+            </section>
+        </aside>
+        <main>
+            <article>
+                <h2>Poster un message</h2>
+                <?php
+                /**
+                 * BD
+                 */
+                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                /**
+                 * Récupération de la liste des auteurs
+                 */
+                $listAuteurs = [];
+                $laQuestionEnSql = "SELECT * FROM users";
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                while ($user = $lesInformations->fetch_assoc()) {
+                    $listAuteurs[$user['id']] = $user['alias'];
+                }
+
+
+                /**
+                 * TRAITEMENT DU FORMULAIRE
+                 */
+                // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
+                // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                $enCoursDeTraitement = isset($_POST['auteur']);
+                if ($enCoursDeTraitement) {
+                    // on ne fait ce qui suit que si un formulaire a été soumis.
+                    // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                    // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                    //echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                    // et complétez le code ci dessous en remplaçant les ???
+                    $authorId = $_POST['auteur'];
+                    $postContent = $_POST['message'];
+
+
+                    //Etape 3 : Petite sécurité
+                    // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                    $authorId = intval($mysqli->real_escape_string($authorId));
+                    $postContent = $mysqli->real_escape_string($postContent);
+                    //Etape 4 : construction de la requete
+                    $lInstructionSql = "INSERT INTO posts (id, user_id, content, created, parent_id) "
+                        . "VALUES (NULL, "
+                        . "'" . $authorId . "', "
+                        . "'" . $postContent . "', "
+                        . "NOW(), "
+                        . "NULL);"
+                    ;
+                    //echo $lInstructionSql;
+                    // Etape 5 : execution
+                    $ok = $mysqli->query($lInstructionSql);
+                    if (!$ok) {
+                        echo "Impossible d'ajouter le message: " . $mysqli->error;
+                    } else {
+                        echo "Message posté en tant que : " . $listAuteurs[$authorId];
+                    }
+                }
+                ?>
+                <form action="wall.php" method="post">
+                    <dl>
+                        <dt><label for='auteur'>Auteur</label></dt>
+                        <dd><select name='auteur'>
+                                <?php
+                                foreach ($listAuteurs as $id => $alias)
+                                    echo "<option value='$id'>$alias</option>";
+                                ?>
+                            </select></dd>
+                        <dt><label for='message'>Message</label></dt>
+                        <dd><textarea name='message'></textarea></dd>
+                    </dl>
+                    <input type='submit'>
+                </form>
+            </article>
+            <?php
+            /**
+             * Etape 3: récupérer tous les messages de l'utilisatrice
+             */
+            $laQuestionEnSql = "
+                    SELECT posts.content, posts.created, users.alias as author_name, 
+                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    FROM posts
+                    JOIN users ON  users.id=posts.user_id
+                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
+                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
+                    LEFT JOIN likes      ON likes.post_id  = posts.id 
+                    WHERE posts.user_id='$userId' 
+                    GROUP BY posts.id
+                    ORDER BY posts.created DESC  
+                    ";
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            if (!$lesInformations) {
+                echo ("Échec de la requete : " . $mysqli->error);
+            }
+
+            /**
+             * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+             */
+            while ($post = $lesInformations->fetch_assoc()) {
+
+                //echo "<pre>" . print_r($post, 1) . "</pre>";
+                ?>
+                <article>
+                    <h3>
+                        <time>
+                            <?php echo $post['created'] ?>
+                        </time>
+                    </h3>
+                    <address>par
+                        <?php echo $post['author_name'] ?>
+                    </address>
+                    <div>
+                        <?php echo $post['content'] ?>
+                    </div>
+                    <footer>
+                        <small>♥
+                            <?php echo $post['like_number'] ?>
+                        </small>
+                        <a href="">#
+                            <?php echo $post['taglist'] ?>
+                        </a>
+                    </footer>
+                </article>
+            <?php } ?>
+
+
+        </main>
+    </div>
+</body>
+
+</html>
