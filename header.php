c3d81f047c0bd64e68ea5c96459534f3b46abe19
Alice <darondeau.alice@gmail.com>
2023-02-20 16:17:12 +0100

    recherche par mots-clés possible et css menu deroulant

--- header.php
+++ header.php
@@ -3,8 +3,35 @@
     <nav id="menu">
         <a href="news.php">Actualités</a>
         <a href="feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Flux</a>
-        <a href="tags.php">Mots-clés</a>
+    </nav>    
+    <nav id="tags">
+        <a href="#">Rechercher un tag</a>
+        <ul>
+            <?php
+            //connexion à la base de données
+            $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+            //verification
+            if ($mysqli->connect_errno) {
+                echo ("Échec de la connexion : " . $mysqli->connect_error);
+            exit();
+            }
+            //création de la requête pour aller chercher tous les tags disponibles
+            $questionTagsSql = "SELECT * FROM tags" ;
+            $infosTags = $mysqli->query($questionTagsSql);
+            while ($tagInfo = $infosTags->fetch_assoc()) {
+                $tagLabel = $tagInfo['label'];
+                $tagId = $tagInfo['id'];
+                ?>
+                <li class="deroulant">
+                    <a href="tags.php?tag_id=<?php echo $tagId ?>"><?php echo $tagLabel ?></a>
+                </li>
+            <?php 
+            }
+            ?>
+        </ul>
+
     </nav>
+
     <nav id="user">
         <a href="#">Profil</a>
         <ul>
